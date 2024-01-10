f6360ba23986670c614b3f6228ff749102de477e
odongo <ronaldgit11@gmail.com>
2024-01-10 13:00:14 +0300

    Task 0.Betty would be proud

--- betty_style.c
+++ betty_style.c
@@ -1,41 +1,15 @@
 #include <stdio.h>
 
 /**
- * Function to calculate the factorial of a number.
+ * main - Entry point of the program
  *
- * @param n The non-negative integer for which to calculate the factorial.
- * @return The factorial of n, or -1 if n is negative.
- */
-int factorial(int n)
-
-	if (n < 0)
-
-	return (-1);
-
-	else if (n == 0)
-{
-	return (1);
-}
-else
-{
-	return (n * factorial(n - 1));
-}
-
-/**
- * Main function -  drive the factorial calculation and user interaction.
+ * Return: 0 on success
  */
 int main(void)
 {
 
-int number;
-	printf("Enter a non-negative integer: ");
-	scanf("%d", &number);
-int result = factorial(number);
-	if (result == -1)
-	printf("Factorial is not defined for negative numbers.\n");
+	printf("Hello, Betty!\n");
 
-else
-printf("The factorial of %d is %d\n", number, result);
-
+	/* Return 0 to indicate successful execution */
 	return (0);
 }
